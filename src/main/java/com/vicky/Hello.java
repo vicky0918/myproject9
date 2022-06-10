8d1de0216f91a3bd6512bc8bc5e9518d73586bbc
vicky0918 <93139591+vicky0918@users.noreply.github.com>
2022-06-10 14:50:13 +0800

    Hello world with class Person

--- src/main/java/com/vicky/Hello.java
+++ src/main/java/com/vicky/Hello.java
@@ -1,4 +1,8 @@
 package com.vicky;
 
+
 public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world~");
+    }
 }
---
+++ src/main/java/com/vicky/Person.java
@@ -0,0 +1,9 @@
+package com.vicky;
+
+public class Person {
+    public void greeting(){
+        //System.out.println("Hello!!");
+        Person p = new Person();
+        p.greeting();
+    }
+}
