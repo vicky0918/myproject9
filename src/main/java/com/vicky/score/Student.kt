792430c21a17f456f9455bb3d93762c5de10a055
vicky0918 <93139591+vicky0918@users.noreply.github.com>
2022-06-17 15:06:43 +0800

    Kotlin Student

---
+++ src/main/java/com/vicky/score/Student.kt
@@ -0,0 +1,19 @@
+package com.vicky.com.vicky.score
+
+class Student(var name:String,var english:Int,var math:Int) {
+    init {
+       //println("Testing")
+    }
+    fun print(){
+        println("$name\t$english\t$math\t${getAverage()}")
+    }
+    fun getAverage() : Int{
+        return(english+math)/2
+    }
+}
+
+fun main(){
+    val stu = Student("Vicky",44,90)
+
+    stu.print()
+}
