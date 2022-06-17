921b424245e3a7c4dbd3c1ae4b8720e52f36503d
vicky0918 <93139591+vicky0918@users.noreply.github.com>
2022-06-17 16:29:34 +0800

    6/17 class

--- src/main/java/com/vicky/score/Student.kt
+++ src/main/java/com/vicky/score/Student.kt
@@ -7,9 +7,7 @@ class Student(var name:String,var english:Int,var math:Int) {
     fun print(){
         println("$name\t$english\t$math\t${getAverage()}")
     }
-    fun getAverage() : Int{
-        return(english+math)/2
-    }
+    fun getAverage() = (english+math)/2
 }
 
 fun main(){
